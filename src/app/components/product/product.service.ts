d7da6d06e14370a4d894246a0d6eae1f8b58ee7a
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2020-12-30 17:24:56 -0400

    atualização de títulos

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -35,22 +35,35 @@ export class ProductService {
     this.showMessage('Ocorreu um erro !',true)
     return   EMPTY ;
   }
+
   read() : Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   readById(id:string): Observable <Product>{
     const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   update(product: Product): Observable<Product>{
     const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   delete(id: number) : Observable<Product>{
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 }
